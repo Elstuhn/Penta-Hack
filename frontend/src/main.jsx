8ea296d736631b4459d3447e998ff6764795df54
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-04-05 00:22:40 +0800

    added search and highlights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,13 +7,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-// ]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,42 +1,86 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-console.log(import.meta.env.VITE_URL, import.meta.env.VITE_KEY)
+console.log(import.meta.env.VITE_URL, import.meta.env.VITE_KEY);
 
-const supabase = createClient(import.meta.env.VITE_URL, import.meta.env.VITE_KEY);
+const supabase = createClient(
+  import.meta.env.VITE_URL,
+  import.meta.env.VITE_KEY
+);
 
 function Home() {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchTemp, setSearchTemp] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [school, setSchool] = useState("");
+  const [topic, setTopic] = useState("");
   const [data, setData] = useState([]);
-  const [file, setFile] = useState('')
+  const [file, setFile] = useState("");
 
-  async function getData() {
-    let { data: post, error } = await supabase.from("post").select("*");
+  useEffect(() => {
+    setIsLoading(true);
+    const timer = setTimeout(() => {
+      if (searchTemp === searchValue) return setIsLoading(false);
+
+      setSearchValue(searchTemp);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchTemp]);
+
+  async function getData(search) {
+    let { data: post, error } = await supabase
+      .from("post")
+      .select()
+      .or(
+        `data.ilike.%${search}%,topic.ilike.%${search}%,sub.ilike.%${search}%,sch.ilike.%${search}%`
+      );
 
     if (error) {
       console.log("Error fetching data:", error);
     } else {
       setData(post);
     }
+    setIsLoading(false);
   }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData(searchValue);
+  }, [searchValue]);
+
   const handleFileUpload = async () => {
     try {
       const { data, error } = await supabase.storage
-        .from('post')
-        .upload('hi', file);
+        .from("post")
+        .upload("hi", file);
 
       if (error) {
         throw error;
       }
 
-      console.log('File uploaded successfully', data);
+      console.log("File uploaded successfully", data);
     } catch (error) {
-      console.error('Error uploading file', error);
+      console.error("Error uploading file", error);
     }
-  };console.log(file)
+  };
+  console.log(file);
+
+  const highlightSubstrings = (text) => {
+    if (!searchValue) {
+      return text;
+    }
+
+    const regex = new RegExp(`(${searchValue})`, "gi");
+
+    return text.split(regex).map((part, index) =>
+      part.toLowerCase() === searchValue.toLowerCase() ? (
+        <span key={index} className="text-yellow-500">
+          {part}
+        </span>
+      ) : (
+        <React.Fragment key={index}>{part}</React.Fragment>
+      )
+    );
+  };
 
   return (
     <div
@@ -45,24 +89,50 @@ function Home() {
     >
       <div>
         <div className="flex flex-row">
-          <h1 className="font-bold text-5xl">Greetings!</h1>
-          <label htmlFor="my-modal-4" className="ml-80 btn">
+          <div className="form-control w-full">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-full"
+              value={searchTemp}
+              onChange={(e) => {
+                setSearchTemp(e.target.value);
+              }}
+            />
+          </div>
+
+          <label htmlFor="my-modal-4" className="ml-2 btn">
             Submit a Request
           </label>
         </div>
+
         {data.map((item, index) => (
           <div
             key={index}
-            className="card w-full md:w-100 bg-base-100 shadow-xl mt-10"
+            className={`card w-full md:w-100 bg-base-100 shadow-xl mt-10  ${
+              isLoading ? "animate-pulse" : ""
+            }`}
           >
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">{item.sch}</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores, magnam! Magnam facilis iure laudantium esse dolorum
-                tenetur recusandae dolor aut a voluptatum, tempore fuga corrupti
-                illum. Ad maxime autem eos.
-              </p>
+            <div className="card-body">
+              <div className="flex justify-between">
+                <span className="card-title justify-self-start">
+                  {
+                    <>
+                      <p className="font-medium">
+                        {highlightSubstrings(item.topic)}
+                      </p>
+                    </>
+                  }
+                </span>
+                <span className="justify-self-end badge badge-info">
+                  {item.sub}
+                </span>
+              </div>
+
+              <p className=""> {highlightSubstrings(item.data)}</p>
+              <div className="flex justify-end font-semibold">
+                <span className="justify-self-end">{item.sch}</span>
+              </div>
             </div>
           </div>
         ))}
@@ -71,16 +141,42 @@ function Home() {
       <input type="checkbox" id="my-modal-4" className="modal-toggle" />
       <label htmlFor="my-modal-4" className="modal cursor-pointer">
         <label className="modal-box relative" htmlFor="">
-          <h3 className="text-lg font-bold">
-            Create new post
-          </h3>
-          <p className="py-4">
-            hello world
-          </p>
-          <input type="file" onChange={e=>{
-            setFile(e.target.files[0])
-          }} name="file" id="file" />
-          <button onClick={()=>handleFileUpload()}>Upload</button>
+          <h2 className="text-xl font-bold mb-3">Create new post</h2>
+          <input
+            type="text"
+            placeholder="School"
+            className="input input-bordered w-full my-3"
+            value={school}
+            onChange={(e) => {
+              setSchool(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Topic"
+            className="input input-bordered w-full my-3 mb-6"
+            value={topic}
+            onChange={(e) => {
+              setTopic(e.target.value);
+            }}
+          />
+          <div className="flex justify-between">
+            <input
+              type="file"
+              onChange={(e) => {
+                setFile(e.target.files[0]);
+              }}
+              className="file-input w-full max-w-xs"
+              name="file"
+              id="file"
+            />
+            <button
+              className="btn btn-primary justify-self-end"
+              onClick={() => handleFileUpload()}
+            >
+              Upload
+            </button>
+          </div>
         </label>
       </label>
     </div>
