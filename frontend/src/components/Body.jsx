ff8caaddb9a7e725069703aa9f2424b65cde5978
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-04-04 15:48:23 +0800

    fadsg

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient("https://cwoqhbnsiqcvlwypmeaq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b3FoYm5zaXFjdmx3eXBtZWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA1ODY5ODcsImV4cCI6MTk5NjE2Mjk4N30.9OTliAVhZWiJci3r9W_CR8ORBzL2GSg3WtmnW2FumlQ");
+const supabase = createClient(
+  "https://cwoqhbnsiqcvlwypmeaq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b3FoYm5zaXFjdmx3eXBtZWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA1ODY5ODcsImV4cCI6MTk5NjE2Mjk4N30.9OTliAVhZWiJci3r9W_CR8ORBzL2GSg3WtmnW2FumlQ"
+);
 
 function Body() {
   const [data, setData] = useState([]);
 
   async function getData() {
-   
-    let { data: post, error } = await supabase .from('post') .select('id')
+    let { data: post, error } = await supabase.from("post").select("id");
 
-    console.log(post)
-    
+    console.log(post);
+
     if (error) {
       console.log("Error fetching data:", error);
     } else {
