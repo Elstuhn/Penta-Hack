065e4353114429f6c522194291287e6e5c291332
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-04-04 15:17:34 +0800

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,23 @@ import { createClient } from "@supabase/supabase-js";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Forum from "./pages/Forum";
+import Create from "./pages/Create";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
