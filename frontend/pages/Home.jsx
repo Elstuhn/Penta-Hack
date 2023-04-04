aee3331174a286c0b02d6e0a9210c4f1410ecdcc
Timothy Chia <timothycth9@gmail.com>
2023-04-04 15:18:02 +0800

    hello

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -3,15 +3,16 @@ import { createClient } from "@supabase/supabase-js";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Body from "./components/Body";
-function App() {
 
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />  
-    </div>
-  );
-}
+function Home() {
 
-export default App;
+    return (
+      <div className="App">
+        <Header />
+        <Body />
+        <Footer />  
+      </div>
+    );
+  }
+  
+  export default App;
--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+
+const supabase = createClient("https://cwoqhbnsiqcvlwypmeaq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b3FoYm5zaXFjdmx3eXBtZWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA1ODY5ODcsImV4cCI6MTk5NjE2Mjk4N30.9OTliAVhZWiJci3r9W_CR8ORBzL2GSg3WtmnW2FumlQ");
+
 function Body() {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
