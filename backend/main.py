6caa21833ae5f56e56676b1f49500249f2a146da
Elston <elston1703@gmail.com>
2023-04-04 15:01:40 +0800

    transformer + flask template

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, session
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello World'
+
+@app.route('/call', methods=["POST"])
+def answer():
+    if request.method == "POST":
+        data = request.data 
+        data = data.decode("utf-8")
+        data = eval(data)
+        try:
+            f, fprime, points = grad_desc(*[data[i] for i in data])
+        except Exception as e:
+            return {"status" : 0, "error": f"{type(e)} {e}"}
+        fprime = str(fprime)
+        return {"f": f, "fprime": fprime, "points": points, "status": 1}
+    else:
+        pass
+
+if __name__ == '__main__':
+    app.run(debug = True)
