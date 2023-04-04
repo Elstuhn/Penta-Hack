6adf92c3d8c12659e3d93af792cb4e8861cf595b
Timothy Chia <timothycth9@gmail.com>
2023-04-04 14:30:44 +0800

    timmy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,17 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const supabase = createClient("https://cwoqhbnsiqcvlwypmeaq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3b3FoYm5zaXFjdmx3eXBtZWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA1ODY5ODcsImV4cCI6MTk5NjE2Mjk4N30.9OTliAVhZWiJci3r9W_CR8ORBzL2GSg3WtmnW2FumlQ");
 
 function App() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
 
   return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
+    <div className="App">
+      <Header />
+      <Footer />  
+    </div>
   );
 }
 
