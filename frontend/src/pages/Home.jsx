a15770e8cf8b236db0fc7251470d933746ecbae8
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-04-07 10:49:11 +0800

    lol

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,16 @@ function Home() {
   const [topic, setTopic] = useState("");
   const [subject, setSubject] = useState("");
   const [data, setData] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
   const [file, setFile] = useState("");
+  const [selectedData, setSelectedData] = useState(null);
+
+  const handleOpen = (data) => {
+    setSelectedData(data);
+    setTimeout(() => {
+      document.getElementById("my-modal-5").checked = true;
+    }, 100);
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -30,17 +39,20 @@ function Home() {
   }, [searchTemp]);
 
   async function getMyPosts() {
-    let { data: myPosts, error } = await supabase
-      .from("post")
-      .select("sch,topic,sub,data,users(username)")
-      .eq("users.id", auth.user.id);
-    console.log("ledata", myPosts);
+    try {
+      let { data: myPosts, error } = await supabase
+        .from("post")
+        .select("id,sch,topic,sub,data,users(username)")
+        .eq("users.id", auth.user.id);
 
-    if (error) {
-      toast.error("Error fetching data");
-      console.log("Error fetching data:", error);
-    } else {
+      if (error) {
+        throw error;
+      }
       console.log(myPosts);
+      setMyPosts(myPosts);
+    } catch (error) {
+      toast.error("Error fetching data");
+      console.log(error);
     }
   }
 
@@ -65,6 +77,7 @@ function Home() {
 
   useEffect(() => {
     if (auth?.user) getMyPosts();
+    if (!auth?.user) setMyPosts([]);
   }, [auth?.user]);
 
   useEffect(() => {
@@ -153,133 +166,161 @@ function Home() {
   };
 
   return (
-    <div
-      className="bg-slate-500 grid grid-cols-2 gap-6 p-10"
-      style={{ gridTemplateColumns: "70% 30%" }}
-    >
-      <div>
-        <div className="flex flex-row">
-          <div className="form-control w-full">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-full"
-              value={searchTemp}
-              onChange={(e) => {
-                setSearchTemp(e.target.value);
-              }}
-            />
+    <>
+      <div
+        className="bg-slate-500 grid grid-cols-2 gap-6 p-10"
+        style={{ gridTemplateColumns: "70% 30%" }}
+      >
+        <div>
+          <div className="flex flex-row">
+            <div className="form-control w-full">
+              <input
+                type="text"
+                placeholder="Search"
+                className="input input-bordered w-full"
+                value={searchTemp}
+                onChange={(e) => {
+                  setSearchTemp(e.target.value);
+                }}
+              />
+            </div>
+
+            {auth?.user && (
+              <label htmlFor="my-modal-4" className="ml-2 btn">
+                Submit a Request
+              </label>
+            )}
           </div>
 
-          {auth?.user && (
-            <label htmlFor="my-modal-4" className="ml-2 btn">
-              Submit a Request
-            </label>
-          )}
-        </div>
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className={`card w-full md:w-100 bg-base-100 shadow-xl mt-10  ${
+                isLoading ? "animate-pulse" : ""
+              }`}
+            >
+              <div className="card-body">
+                <div className="flex justify-between">
+                  <span className="card-title justify-self-start">
+                    {
+                      <>
+                        <p className="font-medium text-xl">
+                          {highlightSubstrings(item.topic)}
+                        </p>
+                      </>
+                    }
+                  </span>
+                  <span className="justify-self-end badge badge-info">
+                    {item.sub}
+                  </span>
+                </div>
 
-        {data.map((item, index) => (
-          <div
-            key={index}
-            className={`card w-full md:w-100 bg-base-100 shadow-xl mt-10  ${
-              isLoading ? "animate-pulse" : ""
-            }`}
-          >
-            <div className="card-body">
-              <div className="flex justify-between">
-                <span className="card-title justify-self-start">
-                  {
-                    <>
-                      <p className="font-medium text-xl">
-                        {highlightSubstrings(item.topic)}
-                      </p>
-                    </>
-                  }
-                </span>
-                <span className="justify-self-end badge badge-info">
-                  {item.sub}
-                </span>
-              </div>
-
-              <p className="mb-5"> {highlightSubstrings(item.data)}</p>
-              <div className="flex justify-between font-semibold">
-                <span className="justify-self-end">
-                  <span className="font-light">Posted by </span>
-                  {item.users.username}
-                </span>
-                <span className="justify-self-end">{item.sch}</span>
+                <p className="mb-5"> {highlightSubstrings(item.data)}</p>
+                <div className="flex justify-between font-semibold">
+                  <span className="justify-self-end">
+                    <span className="font-light">Posted by </span>
+                    {item.users.username}
+                  </span>
+                  <span className="justify-self-end">{item.sch}</span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <div>
-        <div className="card bg-neutral text-neutral-content">
-          <div className="card-body">
-            <h2 className="card-title">My Posts</h2>
-          </div>
+          ))}
         </div>
-      </div>
-
-      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-      <label htmlFor="my-modal-4" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h2 className="text-xl font-bold mb-3">Create new post</h2>
-          <input
-            type="text"
-            placeholder="School"
-            className="input input-bordered w-full my-3"
-            value={school}
-            onChange={(e) => {
-              setSchool(e.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Subject"
-            className="input input-bordered w-full my-3"
-            value={subject}
-            onChange={(e) => {
-              setSubject(e.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Topic"
-            className="input input-bordered w-full my-3 mb-6"
-            value={topic}
-            onChange={(e) => {
-              setTopic(e.target.value);
-            }}
-          />
-          <div className="flex justify-between">
-            <input
-              type="file"
-              accept=".pdf"
-              onChange={(e) => {
-                if (e.target.files[0].type !== "application/pdf") {
-                  e.target.value = null;
-                  return toast.error("Only pdf files are allowed");
-                }
-
-                setFile(e.target.files[0]);
-              }}
-              className="file-input w-full max-w-xs"
-              name="file"
-              id="file"
-            />
-            <button
-              className={`btn btn-primary justify-self-end ${
-                isUploading ? "loading" : ""
-              }`}
-              onClick={() => handleFileUpload()}
-            >
-              Upload
-            </button>
+        <div>
+          <div className="card bg-neutral text-neutral-content mb-5">
+            <div className="card-body">
+              {auth?.user ? (
+                <h2 className="card-title">My Posts</h2>
+              ) : (
+                <h2 className="card-title">Login to see your posts</h2>
+              )}
+            </div>
           </div>
+          {/* Map my posts by their topics and make them clickable which will open a model for more details */}
+          {myPosts.map((post) => (
+            <div key={post.id}>
+              <label
+                className="btn btn-ghost w-full mb-2"
+                onClick={() => handleOpen(post)}
+              >
+                <span className="text-zinc-800">{post.topic}</span>
+              </label>
+            </div>
+          ))}
+        </div>
+
+        <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+        <label htmlFor="my-modal-4" className="modal cursor-pointer">
+          <label className="modal-box relative" htmlFor="">
+            <h2 className="text-xl font-bold mb-3">Create new post</h2>
+            <input
+              type="text"
+              placeholder="School"
+              className="input input-bordered w-full my-3"
+              value={school}
+              onChange={(e) => {
+                setSchool(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Subject"
+              className="input input-bordered w-full my-3"
+              value={subject}
+              onChange={(e) => {
+                setSubject(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Topic"
+              className="input input-bordered w-full my-3 mb-6"
+              value={topic}
+              onChange={(e) => {
+                setTopic(e.target.value);
+              }}
+            />
+            <div className="flex justify-between">
+              <input
+                type="file"
+                accept=".pdf"
+                onChange={(e) => {
+                  if (e.target.files[0].type !== "application/pdf") {
+                    e.target.value = null;
+                    return toast.error("Only pdf files are allowed");
+                  }
+
+                  setFile(e.target.files[0]);
+                }}
+                className="file-input w-full max-w-xs"
+                name="file"
+                id="file"
+              />
+              <button
+                className={`btn btn-primary justify-self-end ${
+                  isUploading ? "loading" : ""
+                }`}
+                onClick={() => handleFileUpload()}
+              >
+                Upload
+              </button>
+            </div>
+          </label>
         </label>
-      </label>
-    </div>
+      </div>
+      {selectedData && (
+        <div className="flex w-full">
+          <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+          <label htmlFor="my-modal-5" className="modal cursor-pointer w-full">
+            <label className="modal-box relative max-w-screen-xl" htmlFor="">
+              <h3 className="text-lg font-bold">{selectedData?.topic}</h3>
+              <p className="py-4 break-words">{selectedData?.data}</p>
+            </label>
+          </label>
+        </div>
+      )}
+    </>
   );
 }
 
